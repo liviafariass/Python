66d243c020d23ac955b5f62ca1425fefe1f476dc
liviafariass <liviafariaas27@gmail.com>
2025-09-08 12:42:34 -0300

    Atualização

--- exercicios/exercicio50.py
+++ exercicios/exercicio50.py
@@ -1,4 +1,21 @@
 #import random
-#random.randint(1,4)
-from modulo import PI 
-print(PI)
+#random.randint(1,4) pega um numero inteiro aleatorio entre 1 e 4
+import modulo #usa pra tudo do modulo
+#from modulo import PI, saudacao -- usa pra algum especifico dentro do modulo, nao todos. Nao precisa do 'modulo.', so o (PI)
+
+def soma(*args):
+    soma = 0
+    for num in args:
+        soma += num
+    return soma
+
+print(modulo.PI)
+print(modulo.saudacao())
+print(modulo.soma(2, 4))
+print(soma(2, 4, 6, 8))
+
+print(__name__) #__main__ : todo arquivo .py tem uma variável especial chamada __name__. Se importarmos o arquivo como módulo em outro código, o __name__ dele será o nome do arquivo (sem .py)
+print(modulo.__name__)
+
+if __name__ == '__main__':
+    print(soma(2, 4, 6, 8))
--- exercicios/modulo.py
+++ exercicios/modulo.py
@@ -6,3 +6,5 @@ def soma(x,y):
     return x + y
 
 PI = 3.14 
+
+print('teste')
